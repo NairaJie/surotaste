a5150c49d36f903dd6971ec60c333084893fd973
nairajie <nairaxaviera@gmail.com>
2025-12-19 03:15:05 +0700

    kecil banget

--- Server/setup/sequelize.js
+++ Server/setup/sequelize.js
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 const adminSequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS || "",
+  process.env.DB_PASS,
   {
     host: "127.0.0.1",
     dialect: "mysql",
