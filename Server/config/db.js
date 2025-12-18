218c1b6f54cfed83fabae9b0ed06d77aa4d01ba3
nairajie <nairaxaviera@gmail.com>
2025-12-19 03:22:41 +0700

    tipis tipis

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,10 +1,11 @@
-import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
 
 const db = mysql.createPool({
-  host: "127.0.0.1", // 🔥 sama
-  user: "root",
-  password: "",
-  database: "surotaste",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 export default db;
