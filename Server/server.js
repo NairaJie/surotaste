0224b9b3f1a9f48df3cb6f94ec12677759d98e2c
nairajie <nairaxaviera@gmail.com>
2025-12-19 03:43:34 +0700

    sumpah terakhir

--- Server/server.js
+++ Server/server.js
@@ -24,7 +24,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 5050;
+const PORT = process.env.PORT || 5050;
 
 // Middleware
 app.use(cors({
